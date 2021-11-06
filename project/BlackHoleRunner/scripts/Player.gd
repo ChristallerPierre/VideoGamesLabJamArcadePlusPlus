398fc6ffdb65d69abbd5c6c7b4de4c448f696c5a
Elemas <pierre.christaller@gmail.com>
2021-11-06 13:21:34 +0100

    0g movements

--- project/BlackHoleRunner/scripts/Player.gd
+++ project/BlackHoleRunner/scripts/Player.gd
@@ -5,32 +5,49 @@ extends Area2D
 # var a = 2
 # var b = "text"
 #Vector2 vector = null
-export var speed = 400
-var player_speed = 0.5
+var player_speed = 50
 var shot_timer
-# shoot every x ms
-var default_shot_timer = 500
-var shot_timer_increment = 1
+# x to 0, decremented by shot_timer_increment*delta each frame
+var default_shot_timer = 1
+var shot_timer_increment = 3
+export var velocity = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.position = OS.get_real_window_size() / 2
-	shot_timer = default_shot_timer
+	shot_timer = 0
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	move_player(delta)
+	check_for_input(delta)
+	pass
+
+func check_for_input(delta):
 	if is_shot_ready(delta):
-		var velocity = read_input()
-		if velocity.length() > 0:
+		var new_velocity = read_input()
+		if is_velocity_changed(velocity, new_velocity):
 			shot_timer = default_shot_timer
-			
-			velocity = velocity.normalized() * speed
-			position += velocity * delta
-			
-			var screen_size = get_viewport_rect().size
-			position.x = clamp(position.x, 0, screen_size.x)
-			position.y = clamp(position.y, 0, screen_size.y)
+	pass
+
+func is_velocity_changed(old_velocity, new_velocity):
+	if new_velocity != Vector2():
+#		if new_velocity.x != 0 :
+		velocity += new_velocity
+		return true
+	return false
+
+func move_player(delta):
+	if velocity.length() <= 0:
+		pass
+	
+#	velocity = velocity * player_speed
+	position += velocity * delta
+	
+	var screen_size = get_viewport_rect().size
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
 	pass
 
 # don't call this until animations are implemented
