8216eb5c326e5d8c4c2537d094680bcce94ad1a9
ChristallerPierre <pierre.christaller@gmail.com>
2021-11-07 21:39:36 +0100

    scene menu renamed + game over displays

--- project/BlackHoleRunner/scripts/Main.gd
+++ project/BlackHoleRunner/scripts/Main.gd
@@ -1,23 +1,38 @@
 extends Node
 
-const base_score = "Score "
+const base_score = "Score : "
+const text_r_to_reload = "\nAppuyez sur R pour recommencer une partie."
+const reload_input = "reload"
 export (PackedScene) var Mob
 export var score = 0
+var game_over
 
 func _ready():
-	randomize()
-	var window_dimensions = OS.get_real_window_size()
-	$Score.rect_position.x = window_dimensions.x / 2 - 50
-	$Score.rect_position.y = 50
 	new_game()
+	game_over = false
 
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	game_over = true
+	draw_game_over()
+	$Score.text = ""
+	$Player.hide()
+
+func _input(event):
+	if event.is_action_pressed(reload_input) && game_over:
+		print("reloading game")
+		get_tree().reload_current_scene()
 
 func new_game():
+	randomize()
+	var window_dimensions = OS.get_real_window_size()
+	$Score.rect_position.x = window_dimensions.x / 2 - 50
+	$Score.rect_position.y = 50
 	score = 0
 	$StartTimer.start()
+	$GameOverLabel.text = ""
+	$GameOverSprite.hide()
 	
 func _on_StartTimer_timeout():
 	$MobTimer.start()
@@ -46,6 +61,11 @@ func _on_MobTimer_timeout():
 	# Set the velocity (speed & direction).
 	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
 	mob.linear_velocity = mob.linear_velocity.rotated(direction)
+
+func draw_game_over():
+	var text = base_score + str(score) + text_r_to_reload
+	$GameOverLabel.text = text
+	$GameOverSprite.visible = true
 
 func draw_score():
 	var text = base_score + str(score)
--- project/BlackHoleRunner/scripts/Menu.gd
+++ project/BlackHoleRunner/scripts/Menu.gd
@@ -3,7 +3,7 @@ extends Control
 func _ready():
 	pass # Replace with function body.
 func _process(delta):
-	$Sprite2.rotation_degrees += delta*20
+	$BlackHole.rotation_degrees += delta*20
 
 func _on_Start_pressed():
 	get_tree().change_scene("res://scenes/Main.tscn")
