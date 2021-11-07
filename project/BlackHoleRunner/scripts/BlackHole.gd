cdbfd2d29df9f5660a34d282489392f53ebe08d6
Elemas <pierre.christaller@gmail.com>
2021-11-07 13:12:25 +0100

    moved black hole script

--- project/BlackHoleRunner/scripts/BlackHole.gd
+++ project/BlackHoleRunner/scripts/BlackHole.gd
@@ -1,5 +1,6 @@
 extends Area2D	
 
 func _on_BlackHole_body_entered(body):
+	print(typeof(body))
 	print("Dans le trou noir")
 	pass # Replace with function body.
--- project/BlackHoleRunner/scripts/Monster.gd
+++ project/BlackHoleRunner/scripts/Monster.gd
@@ -9,7 +9,7 @@ func _ready():
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
 
-func _on_Monster_body_entered(body):
+func _on_Monster_body_entered(_body):
 	print("mob tué")
 	hide()
-	pass # Replace with function body.
+	# increment score
